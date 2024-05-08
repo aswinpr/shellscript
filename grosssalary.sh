1340cf152151b55cf47966cf9edea2c037f14663
aswin_pr <97214080+aswinpr@users.noreply.github.com>
2024-05-08 06:59:09 +0530

    grosssalary.sh

--- grosssalary.sh
+++ grosssalary.sh
@@ -1,16 +1,22 @@
-#!/bin/bash
 calculate_gross_salary() {
-local basic=$1
-if [ $basic -lt 15000 ]; then
-hra=$(echo "scale=2; $basic * 0.10" | bc)
-da=$(echo "scale=2; $basic * 0.90" | bc)
-else
-hra=100
-da=$(echo "scale=2; $basic * 0.98" | bc)
-fi
-gross_salary=$(echo "scale=2; $basic + $hra + $da" | bc)
-echo $gross_salary
+    basic_salary=$1
+    
+    if [ $basic_salary -gt 15000 ]; then
+        hra_percent=10
+        da_percent=90
+    else
+        hra_percent=
+        da_percent=
+    fi
+    
+    hra=$((basic_salary * hra_percent / 100))
+    da=$((basic_salary * da_percent / 100))
+    gross_salary=$((basic_salary + hra + da))
+    
+    echo $gross_salary
 }
-read -p "Enter the basic salary: " basic_salary
+echo "Enter basic salary of the employee:"
+read basic_salary
+
 gross_salary=$(calculate_gross_salary $basic_salary)
-echo "Gross Salary: $gross_salary"
+echo "Gross salary of the employee is: $gross_salary"
